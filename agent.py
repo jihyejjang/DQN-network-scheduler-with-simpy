c3a4e7575e0125b8f44e6f0830d56869cc2ff762
Jihye <jh_r_1004@naver.com>
2020-11-13 17:07:25 +0900

    commit 1113_2

---
+++ agent.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Reinforcement learning Simpy Env&Agent
+
+# In[8]:
+
+
+import simpy
+import pandas as pd
+import numpy as np 
+import random 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+#from prettytable import PrettyTable
+from collections import deque 
+from itertools import product
+import math
+from dqn import DQN
+import time
+
+#보상 함수1: deadline이 적게 남을수록 pacing rate을 많이 할당    
+def reward_function(deadlines,action,value): 
+    reward = 0
+    drem_max=np.max(deadlines)
+    value=np.array(value)
+    active=np.where(value==None) #active flow index
+    act_deadline=[] #active flow deadline
+    
+    try: #active(전송중인) flow에 대해
+        for a in active[0]:
+            act_deadline.append(deadlines[a])
+        drem_max=np.max(act_deadline) #최대 deadline을 가지는 active flow
+    except: #deactive flow만 있을 때
+        drem_max=np.max(deadlines)
+        
+        
+    for dremi in range(len(deadlines)): #active한 flow, deadline이 작을수록 action이 많이 할당되어야 reward또한 커짐
+        if (value[dremi]==None): #active
+            reward += ((drem_max - deadlines[dremi])*action[dremi])
+        else: #deactive
+            reward += 0
+        
+    return reward
+
+    
+def episode(env,DQN,Tsc,Tfu,allowed_actions): #pacing rate가 각 flow에게 할당
+    global action
+    global state
+    global request
+    global flow_success
+    global record
+    
+    
+    m=-200 # 200번의 시행 동안
+    s=0.9 # 성공률이 90%이상이면
+    
+    cnt=1 #episode 수
+    c=0 #scheduling interval의 수
+   
+    #episode 시작
+    
+    while True: #Simularion time 동안 episode를 반복한다
+        print ("--------------------------------------------------")
+        print("********Episode start********",cnt)
+        print ("")
+        
+        terminal=False
+        first_action=1 #new action policy -> EDF
+        
+        #1개의 에피소드는 모든 filesize가 0이 될때까지 실행
+        while ((request['filesize'][0]!=0)or(request['filesize'][1]!=0)or(request['filesize'][2]!=0)):#모두 전송이 완료될 때 까지
+            c+=1
+            
+            #state 결정
+            
+            state=[0,0,0] #state 초기화
+            for s in range(len(sources)):#deadline이 0이 아닌 source는 그대로
+                if ( request['value'][s] != None ) : # (filesize=0 , value =1,0) 
+                    state[s]=0
+                else: #active, value=none
+                    if (request['deadline'][s] > 0) : #(value = None, deadline >0이면 전송중, deadline =0이면 기한 지남)
+                        state[s]=math.ceil(request['filesize'][s]/request['deadline'][s])
+                    elif (request['deadline'][s] == 0):# 기한 지남, 남은시간 0
+                        state[s]=10 #link capacity 전체 할당
+                    elif (request['deadline'][s] < 0):# 기한 지남
+                        state[s]=math.ceil(request['filesize'][s]/request['deadline'][s])
+            
+            print ("state", state)
+
+            #action 결정
+            
+            if (first_action==1): #New state-action policy를 EDF방식으로 (가장 deadline이 시급한)     
+                action=[0,0,0]
+                index=np.argmin(request['deadline']) #deadline 최소인 source의 index
+                action[index]=10 #bottleneck capacity
+                
+            else: 
+                action,p,select=dqn_agent.choose_action(state,allowed_actions) #DQN에 의한 액션선택
+                pexp.append(p) #epsilon값
+                
+            print ("action", action)
+                
+            #Scheduling interval 시작
+            print ("Tsc" , c)
+            
+            for i in range(Tsc): 
+                for s in range(len(sources)): #각 source에 대해 
+                    
+                    #filesize와 deadline 감소
+                    
+                    request['filesize'][s]=max([request['filesize'][s]-int(action[s]),0]) #filesize는 음수 X
+                    
+                    if (request['value'][s]==None):
+                        request['deadline'][s]= request['deadline'][s]-1 #deadline는 음수 가능->state때문, reward에서는 음수반영 x reward는 deadline안에 전송하는것이 목적이기 때문임
+                    
+                    # Active, Deactive flow 검사, 전송완료되면 deadline 0으로.
+                    
+                    if ((request['filesize'][s]==0) and (request['value'][s]==None)): #아직 완료되지 않았던 flow가 전송이 완료되면?
+                        
+                        if (request['deadline'][s]>=0): #기간 안에 전송되면? 남아있는 시간이 양수, 또는 0 (시간이 0에 딱 맞게 전송 되는 경우도 있음..)
+                            request['value'][s]=1 #value를 1로 변경
+                            request['deadline'][s] = 0 #남은시간도 0
+                            flow_success.append(1)
+                            #print ("s{}의 전송이 deadline 안에 완료됨".format(s))
+                            
+                        else: #기간안에 전송된게 아니라면(value초기값은 None)
+                            request['value'][s] = 0
+                            flow_success.append(0)
+                            #print ("s{}의 전송이 deadline을 지나 완료됨".format(s))
+                            
+                yield env.timeout(Tfu)# Tfu(1초)마다 위 과정 실행
+            
+            
+            
+            #모든 전송이 완료된 후 next_state는 고려할 필요 없음: terminal=True로 하여 target에 reward를 할당
+            
+            if ((request['filesize'][0]==0)and(request['filesize'][1]==0)and(request['filesize'][2]==0)):
+                terminal = True
+            
+            #Next state 결정
+            
+            next_state=[0,0,0]
+            for s in range(len(sources)):
+                if (request['value'][s] == 1) : 
+                    next_state[s]=0
+                else: #active, value=none
+                    if (request['deadline'][s] > 0) : 
+                        next_state[s]=math.ceil(request['filesize'][s]/request['deadline'][s])
+                    elif (request['deadline'][s] == 0):
+                        next_state[s]=10 #link capacity
+                    elif (request['deadline'][s] < 0):
+                        next_state[s]=math.ceil(request['filesize'][s]/request['deadline'][s])
+
+            #print("next_state" , next_state)
+                        
+            reward = reward_function((request['deadline']),action,request['value'])
+            cur_state = state 
+            action = action
+            new_state = next_state 
+            reward = reward
+            terminal = terminal
+            
+            if (first_action==0): # 첫번쨰 선택 액션이 아닌경우에만 학습
+                dqn_agent.remember(cur_state, action, reward, new_state,terminal) #새로운 state로 설정해주고 기존state저장
+                mse_loss.append(dqn_agent.replay(allowed_actions))#학습, loss 저장
+                dqn_agent.train_target()
+                record.append([cur_state, action, reward, new_state , select]) #select는 action을 random에 의해 선택했는지 dqn에 의해 선택했는지 여부
+            
+            first_action=0 
+
+        for i in range(len(sources)):
+            if (request['value'][i]==1):
+                print ("source {} 전송완료".format(i))
+            else:
+                print ("source {} deadine 충족하지 못함".format(i))
+                    
+        if terminal: #학습 종료 시 검사
+            if (np.mean(np.array(flow_success)[:m]) >= s) and (np.all(np.array(select[:m])==True) ):
+                #결과 저장
+                
+                np.savez('simulation history 1106_notebook_adam 1e-3',loss = mse_loss, success = flow_success, p = pexp, record = record )
+                dqn_agent.save_model("dqn_policy.h5")
+                print ("성공률 90%이상, 학습 종료")
+                sys.exit()
+
+        
+        print("모두 전송 완료")
+        
+        #다음 episode에 simulation할 flow생성
+
+        filesize=[random.randrange(10,50)for source in range(len(sources))] #byte 단위
+        deadline=[int(filesize[source]/3) for source in range(len(sources))] #sum_rmin이  9
+        request = {
+            'sources' : sources,
+            'destinations' : destinations, 
+            'filesize' : filesize, #단위는 Gbps
+            'deadline' : deadline,
+            'value' : [None for source in sources] #아직 전송되지 않았으면 None, 제시간에 전송되었으면 1, 제시간에 전송되지 않았으면 0
+        }
+
+        state=[0,0,0]#초기 state
+        action=[0,0,0]#초기 action ->고칠것
+        print ("********episode end********")
+        print("--------------------------------------------------")
+        cnt+=1
+
+#npz
+mse_loss=[] #episode의 진행에 따른 mse_loss의 변화율 graph
+flow_success=[] #episode의 진행에 따른 flow_success rate
+pexp=[] #action choice 진행에 따른 pexp 변화
+record = []
+
+state=[0,0,0]#초기 state
+action=[0,0,0]#초기 action ->고칠것
+Tsc=2 #scheduling interval을 구성하는 flow update interval의 수
+Tfu=1 #flow update interval의 시간단위(0.1초로 가정)
+sources = ['s0', 's1', 's2']
+destinations = ['d0', 'd1', 'd2']
+filesize=[random.randrange(10,50) for source in range(len(sources))] #단위 Gb
+deadline=[int(filesize[source]/3) for source in range(len(sources))] #sum_rmin이  9Gbps, 각각 3
+request = {
+    'sources' : sources,
+    'destinations' : destinations, 
+    'filesize' : filesize, #단위는 Mbps
+    'deadline' : deadline,
+    'value' : [None for source in sources] #아직 전송되지 않았으면 None, 제시간에 전송되었으면 1, 제시간에 전송되지 않았으면 0
+}  
+
+env = simpy.Environment()
+dqn_agent = DQN()
+
+    
+def main(filename):    
+    # main함수
+
+    A=range(11)
+    B=range(11)
+    C=range(11) 
+    allowed_actions=[] #합이 10이 되는 0~10까지의 수 조합
+    for i in product(*(A,B,C)):
+        if sum(i)==10:
+            allowed_actions.append(i) #d는 66개의 조합
+
+    start = time.time()  # 시작 시간 저장
+    env.process(episode(env,DQN,Tsc,Tfu,allowed_actions))
+    env.run(until=400000)#10만 초 동안 가동
+
+    #결과 저장
+    np.savez(filname,loss = mse_loss, success = flow_success, p = pexp, record = record )
+    dqn_agent.save_model("dqn_policy.h5")
+
+    print("종료")
+    print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
+    
+if __name__=="__main__":
+    main(filename)
+
---
+++ dqn.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DQN model
+
+# In[ ]:
+
+
+#dqn model
+
+import pandas as pd
+import numpy as np 
+import random 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from collections import deque 
+from itertools import product
+import math
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam 
+
+class DQN:#모델 선언
+    def __init__(self): #parameter들의 초기값
+        self.epsilon = 0.999
+        self.epsilon_min = 0.01
+        self.step = 1
+        self.tau = 0.125 #?
+        self.learning_rate = 1 #할인율. 1에 가까울 수록 미래에 받는 보상도 중요, 0에 가까울수록 즉각적인 보상이 중요
+        self.memory = deque()
+        self.model = self.create_model() #현재 state에 대한 model
+        self.target_model = self.create_model() #next state에 대한 model
+
+    # create the neural network to train the q function 
+    def create_model(self): #Q값예측모델. 
+        model = Sequential()
+        model.add(Dense(24, input_dim= 3, activation= 'relu')) # input dimension : source들 차원
+        model.add(Dense(48, activation= 'relu'))
+        model.add(Dense(24, activation= 'relu'))
+        model.add(Dense(66)) #계산했을 때,(1~10)까지 세 수의 합이 10이 되는 경우의수는 66개. output에 대한 가중치는 매번 update되기 때문에 이에 mapping시키면 된다. 
+        model.compile(loss= 'mean_squared_error', optimizer= Adam(lr= 1e-3)) #optimizer의 learning rate도 주의
+        
+        return model 
+
+
+
+    # Action function to choose the best action given the q-function if not exploring based on epsilon p값에 의한 예측이 아닐때
+    def choose_action(self, state, allowed_actions): #action을 선택 (parameter로 선택가능한 action이 들어옴)
+        select = False
+        if (self.step%10000 == 0):#약 10만번 step에서 3만번의 scheduling 발생. 10000번마다 epsilon이 감소
+            self.epsilon = max(self.epsilon_min, pow(self.epsilon,int(self.step/10000 +1)))
+        #print ("epsilon", self.epsilon)
+        self.step+=1
+        r = np.random.random()
+        if r < self.epsilon: #p값보다 작은 경우 랜덤한 액션을 취함
+            print("random action")
+            return random.choice(allowed_actions),self.epsilon,select
+        
+        
+        print ("@@action choose@@" , self.step)
+        select = True
+        state = np.array(state).reshape(1,len(state)) #p값보다 큰경우, state 배열 생성
+        
+        pred = self.model.predict(state)[0]
+        #print ("q",pred)
+        
+        return self.maxQ_action(pred,allowed_actions),self.epsilon,select #Q예측값중 min_rate 이상으로 가장 큰 action을 선택
+    
+
+    def maxQ_action(self,pred,allowed_actions):#allowed action 생성 (min_rate 이상 조합만 남김)
+        print ("max q", np.argmax(pred) )
+        return allowed_actions[np.argmax(pred)]
+        
+        
+        
+    # create replay buffer memory to sample randomly #메모리에서 꺼내서 학습할 수 있게 저장, terminal이란 next_state가 없는 경우
+    def remember(self, state, action, reward, next_state,terminal):
+        self.memory.append([state, action, reward, next_state,terminal])
+
+
+    # build the replay buffer 저장한 것을 버퍼에서 꺼내오는.? 학습단계?
+    def replay(self,allowed_actions):
+        
+        #global mse_loss
+        mse=[]
+        batch_size = 32
+        if len(self.memory) < batch_size: #buffer에 저장된 memory가 buffer의 총 batch_size보다 작다면 return
+            return 
+        
+        samples = random.sample(self.memory, batch_size) #메모리에서 배치사이즈만큼 랜덤으로 선택
+        
+        
+        for sample in samples:
+            
+            #print ("sample" , sample)
+            
+            state, action, reward, new_state, terminal = sample # sample 데이터 하나를 꺼내서
+            
+            state = np.array(state).reshape(1,len(state)) 
+            
+            new_state = np.array(new_state).reshape(1,len(new_state))
+            
+            target = self.target_model.predict(state) 
+            
+            action_id = allowed_actions.index(tuple(action)) #63개의 allowed action 중에서 state에 대한 action의 index를 추출
+
+            if terminal :
+                target[0][action_id] = reward
+            else :
+                next_pred = self.target_model.predict(new_state)[0] #new state에 대한 target 예측
+                  
+                Q_future= max(next_pred) #next state에 대한 predict 값 중 가장 큰 값이 Q값이 됨
+                
+                target[0][action_id] = reward + Q_future * self.learning_rate # target의 action_id번째 위치에 다음 Q값이 들어감. 맞춰야 하는 값!!!
+            
+            history=self.model.fit(state, target, epochs= 1, verbose= 0) 
+            mse.append(history.history['loss'][0]) # loss 기록
+            
+        return min(mse)
+        
+        #print("Mean_square_error:"min(mse_loss))
+        
+
+
+    # update our target network 
+    def train_target(self): #target network를 업데이트
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)#loss함수?
+            #target_weights[i] = weights[i]
+        self.target_model.set_weights(target_weights)
+
+
+
+    # save our model 
+    def save_model(self, fn):
+        self.model.save(fn)
+
